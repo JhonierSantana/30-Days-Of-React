233dfa4366c7b003c78ae7b441b400c49f72bf7f
JhonierSantana <Jhonier_2504@hotmail.com>
2023-08-21 16:20:27 -0500

    Exercise level 3 done

--- Day4Level3/src/index.js
+++ Day4Level3/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import imgCreator from './imgs/asabeneh.jpg';
 
 const styleC = {
   display:'flex',
@@ -9,6 +10,37 @@ const styleC = {
   margin: '5px 20px',
   borderRadius: '10px'
 }
+
+const roundedImg = {
+  width: '200px',
+  borderRadius: '50%',
+  marginLeft: '40px'
+}
+const fontsize = {
+  fontSize: '25px',
+  marginLeft: '20px',
+  marginTop: '30px'
+}
+
+const boxStyle = {
+  width: 'auto',
+  height: '50px',
+  backgroundColor: '#07569B', 
+  margin: '5px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white',
+  borderRadius: '10px',
+};
+const containerStyle = {
+  display: 'flex',
+  flexWrap: 'wrap'
+};
+
+const contentStyle = {
+  padding: '5px',
+};
 
 const cajasC = (
   <>
@@ -29,10 +61,35 @@ const cajasC = (
   </div>
   </>
 )
+const skills = [
+  'HTML', 'CSS', 'Sass', 'JS', 'React', 'Redux', 'Node', 'MongoDb', 
+  'Python', 'Flask', 'Django', 'Numpy', 'Pandas', 'Data Analysis', 
+  'MYSQL', 'GraphSql', 'D3.JS', 'Gatsby', 'Docker', 'Heroku', 'Git'
+];
+
+const creator = (
+  <>
+    <div >
+      <img src={imgCreator} style={roundedImg}></img>
+      <h1 style={fontsize}>ASABENEH YETAYEH</h1>
+      <p style={{fontSize: '15px', marginLeft: '20px', marginTop: '20px'}}>Senior Developer Finland</p>
+    </div>
+    <h1 style={{...fontsize}}>SKILLS</h1>
+    
+    <div style={containerStyle}>
+    {skills.map((skill, index) => (
+      <div style={boxStyle} key={index}>
+        <div style={contentStyle}>{skill}</div>
+      </div>
+    ))}
+  </div>
+  </>
+)
 
 const app = (
   <div className='app'>
       {cajasC}
+      {creator}
   </div>
 )
 const rootElement = document.getElementById('root');
